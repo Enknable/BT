afe8cb1d2434b875985cbc3fc2c116d0eb299da1
Andrew <lil_tagger04@hotmail.com>
2013-10-19 21:26:25 -0400

    UDP Server

---
+++ Serv.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define CHUNK_SIZE 1024
+
+int main( int argc, char *argv[])
+{
+
+int status, SendR,  yes=1;
+struct addrinfo hints, *res;
+struct addrinfo *servinfo;  // will point to the results
+
+memset(&hints, 0, sizeof hints); // make sure the struct is empty
+hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+hints.ai_socktype = SOCK_DGRAM; // UDP datagram sockets
+hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+}
+
+SendR = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+if(SendR == 1)
+    fprintf(stderr, "Socket Error: %s\n", strerror(errno));
+
+
+if(bind(SendR, res->ai_addr, res->ai_addrlen) == -1)
+    fprintf(stderr,"Bind Error: %s\n", strerror(errno));
+
+//lose the pesky "Address already in use" error message
+if (setsockopt(SendR,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) {
+    perror("setsockopt");
+    exit(1);
+} 
+
+
+freeaddrinfo(servinfo); // free the linked-list
+
+return 0;
+}
