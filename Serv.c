c791bb5b345a1037f41cb072c20666f1774d82fc
Andrew <lil_tagger04@hotmail.com>
2013-10-20 01:15:30 -0400

    Serv with blocking for udp read

--- Serv.c
+++ Serv.c
@@ -14,22 +14,29 @@
 int main( int argc, char *argv[])
 {
 
-int status, SendR,  yes=1;
-struct addrinfo hints, *res;
-struct addrinfo *servinfo;  // will point to the results
+int status, SendR,  yes=1, fdmax, newfd, i;
+struct addrinfo hints, *res;  // will point to the results
+fd_set master;    // master file descriptor list
+fd_set read_fds;  // temp file descriptor list for select()
+char buf[256];    // buffer for client data
+struct sockaddr_storage remoteaddr; // client address
+socklen_t addrlen;
+
+FD_ZERO(&master);    // clear the master and temp sets
+FD_ZERO(&read_fds);
 
 memset(&hints, 0, sizeof hints); // make sure the struct is empty
 hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
 hints.ai_socktype = SOCK_DGRAM; // UDP datagram sockets
 hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
-if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
+if ((status = getaddrinfo(NULL, "3490", &hints, &res)) != 0) {
     fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
     exit(1);
 }
 
 SendR = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-if(SendR == 1)
+if(SendR == -1)
     fprintf(stderr, "Socket Error: %s\n", strerror(errno));
 
 
@@ -42,8 +49,32 @@ if (setsockopt(SendR,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) {
     exit(1);
 } 
 
+// add the listener to the master set
+    FD_SET(SendR, &master);
 
-freeaddrinfo(servinfo); // free the linked-list
+// keep track of the biggest file descriptor
+    fdmax = SendR; // so far, it's this one
+
+for(;;){
+    read_fds = master; // copy it
+    if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+        perror("select");
+        exit(4);
+    }
+
+    // run through the existing connections looking for data to read
+    for(i = 0; i <= fdmax; i++) {
+        if (FD_ISSET(i, &read_fds)) { // we got one!!
+            if (i == SendR) {
+                // handle new connections
+                addrlen = sizeof remoteaddr;
+
+                recv(i, buf, sizeof buf, 0);
+            }
+        }
+    }
+}
+freeaddrinfo(res); // free the linked-list
 
 return 0;
 }
