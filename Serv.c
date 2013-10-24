1dc7035ba5be79891d7f13048c9d9f6c23134be2
Andrew <lil_tagger04@hotmail.com>
2013-10-24 04:12:41 -0400

    breaky2

--- Serv.c
+++ Serv.c
@@ -133,7 +133,7 @@ for(;;){
             write_fds = master;
             
     if(FD_ISSET(i, &write_fds)){
-            printf("%u\n", bt.sqNum);
+            printf("%u is set\n", bt.sqNum);
             byte_count = sendto(SDARRAY[count-1], "HI", sizeof bt, 0,(struct sockaddr *) remoteaddrudp[count-1], addrlenudp[count-1]);
             if(byte_count==-1)
                 fprintf(stderr, "sendto error: %s\n", gai_strerror(byte_count));
