bb2b440cd99de66124312d1cdba0320de3b94fd0
Andrew <lil_tagger04@hotmail.com>
2013-10-23 23:54:59 -0400

    Recvstruct

--- Recv.c
+++ Recv.c
@@ -35,7 +35,7 @@ int main(){
         fprintf(stderr, "Socket Error: %s\n", strerror(errno));
     
     //craft header, send with seq 0
-    if ((numbytes = sendto(RecvR, "Hi", 2, 0,
+    if ((numbytes = sendto(RecvR, &bt, sizeof(bt), 0,
              res->ai_addr, res->ai_addrlen)) == -1) {
         perror("talker: sendto");
         exit(1);
