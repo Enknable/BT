cc256e9f0483eff155bc18e8ccba0fac60237127
Andrew <lil_tagger04@hotmail.com>
2013-10-24 00:36:42 -0400

    checkitout

--- Recv.c
+++ Recv.c
@@ -14,7 +14,7 @@
 
 int main(){
     int status, RecvR, numbytes;    
-    uint16_t sqNum = 41189; //Max 65535    
+    uint16_t sqNum = 4; //Max 65535    
     struct port bt;
     struct addrinfo hints, *res;  // will point to the results
     bt.sqNum = sqNum;
