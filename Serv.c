4427a8160934c573829daab3ca8cf78306a36326
Andrew <lil_tagger04@hotmail.com>
2013-10-27 12:12:45 -0400

    hi

--- Serv.c
+++ Serv.c
@@ -199,6 +199,7 @@ for(;;){
             //printf("%i\n", remoteaddrudp[count-1]);
             
             
+            
             if(rand() % 10 == 9)
                 //bt.sqNum++;
             byte_count = sendto(SDARRAY[i], &bt, sizeof bt, 0, remoteaddrudp[i], addrlenudp[i]);
