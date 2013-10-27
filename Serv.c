21ad06bb7cc649804cb727d903243d3acf745f64
Andrew <lil_tagger04@hotmail.com>
2013-10-27 12:11:45 -0400

    hi

--- Serv.c
+++ Serv.c
@@ -200,7 +200,7 @@ for(;;){
             
             
             if(rand() % 10 == 9)
-                bt.sqNum++;
+                //bt.sqNum++;
             byte_count = sendto(SDARRAY[i], &bt, sizeof bt, 0, remoteaddrudp[i], addrlenudp[i]);
                 inet_ntop(remoteaddr.ss_family,
                         get_in_addr(remoteaddrudp[i]),
