6c09fcf65da4c13c658c361df422eab162898e53
Andrew <lil_tagger04@hotmail.com>
2013-10-27 15:52:16 -0400

    hi

--- Serv.c
+++ Serv.c
@@ -160,7 +160,7 @@ for(;;){
                         if(bt.sqNum != 0){
                             if(bt.ack == 1){
                             ACK[bt.WHOAMI] = 1;
-                            
+                            sleep(0.01);
                             }else{
                             
                             ACK[bt.WHOAMI] = bt.ack;
