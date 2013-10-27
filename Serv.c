0ed9ba690883f811e14a69f0281dd25534987930
Andrew <lil_tagger04@hotmail.com>
2013-10-27 12:39:23 -0400

    hi

--- Serv.c
+++ Serv.c
@@ -203,10 +203,10 @@ for(;;){
             
             j=rand()%10;
             printf("%i", j);
-                if(j == 1)
+                if(j == 1){
                 SQARRAY[SD];
  [i]  += 1 
-                             bt.sqNum = SQARRAY[SDARRAY[i]];
+                }             bt.sqNum = SQARRAY[SDARRAY[i]];
                 
             byte_count = sendto(SDARRAY[i], &bt, sizeof bt, 0, remoteaddrudp[i], addrlenudp[i]);
                 inet_ntop(remoteaddr.ss_family,
