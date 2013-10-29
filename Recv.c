dacc52ecd2b96889f732ad1217400c82234dd5a2
Andrew <lil_tagger04@hotmail.com>
2013-10-28 20:27:58 -0400

    hi

--- Recv.c
+++ Recv.c
@@ -104,7 +104,7 @@ for(;;){
     }
     for(;;){
         numb_bytes=0;
-numb_bytes += recvfrom(SendR, &bt, sizeof bt, 0,(struct sockaddr *) &remoteaddr, &addrlen);
+ recvfrom(SendR, &bt, sizeof bt, 0,(struct sockaddr *) &remoteaddr, &addrlen);
     if(bt.sqNum != sqNumb){
         if ((numbytes = sendto(RecvR, &bt, sizeof bt, 0,
              res->ai_addr, res->ai_addrlen)) == -1) {
