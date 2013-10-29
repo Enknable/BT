9e31291dde41cea3347d9948942976c425533ad3
Andrew <lil_tagger04@hotmail.com>
2013-10-28 20:15:38 -0400

    hi

--- Serv.c
+++ Serv.c
@@ -217,9 +217,7 @@ for(;;){
                 if(j == 1)
                 SQARRAY[SDARRAY[i]]++;
                 bt.sqNum = SQARRAY[SDARRAY[i]];
-            
-            if(sz / CHUNK_SIZE < SQARRAY[SDARRAY[i]])
-            bt.sz=-1;
+
             bt.sz=sz;    
             bt.WHOAMI = SDARRAY[i];
             byte_count = sendto(SDARRAY[i], &bt, sizeof bt, 0, remoteaddrudp[i], addrlenudp[i]);
