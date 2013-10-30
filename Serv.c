a68982bf3afac30e503cac2d8f1ba85481110177
Andrew <lil_tagger04@hotmail.com>
2013-10-29 21:44:03 -0400

    hi

--- Serv.c
+++ Serv.c
@@ -216,10 +216,10 @@ for(;;){
             
             
         
-            j=(rand()%1000);
-            printf("%i\n", j);
-                if(j == 1)
-                SQARRAY[SDARRAY[i]]++;
+            //j=(rand()%1000);
+            //printf("%i\n", j);
+               // if(j == 1)
+                //SQARRAY[SDARRAY[i]]++;
                 
                 bt.sqNum = SQARRAY[SDARRAY[i]];
                 
