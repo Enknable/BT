c1c7aed99471c6404a7952f4a59aa23e7755c5f3
Andrew <lil_tagger04@hotmail.com>
2013-11-02 02:21:19 -0400

    hi

--- Recv.c
+++ Recv.c
@@ -173,7 +173,7 @@ for(;;){
 
 
         //printf("%s", bt.data);
-        fwrite(bt.data , 1 , strlen(bt.data) , fp );
+        fwrite(bt.data , 1 , strlen(bt.data)-1 , fp );
       //  data_bytes += numb_bytes - 20;
     //if(bt.sz*1000 <= data_bytes)
     //exit(1);
